724880341445fec8a1937a5a16825471aaba20ea
Dario Tecchia <dario.tecchia@nttdata.com>
2024-04-16 14:43:21 +0200

    init

---
+++ app.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import { Validator } from "@marketto/codice-fiscale-utils";
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.use(bodyParser.json());
+
+app.use("*", (req: Request, res: Response, next: NextFunction) => {
+  console.log("Request", req);
+  console.log("Response", res);
+  next();
+});
+
+app.post("/check-fiscal-code", (req: Request, res: Response) => {
+  const fiscalCode: string = req.body.data;
+
+  if (!fiscalCode) {
+    return res.send("false");
+  }
+
+  const isValidFiscalCode = checkFiscalCode(fiscalCode);
+
+  res.send(JSON.stringify(isValidFiscalCode));
+});
+
+function checkFiscalCode(fiscalCode: string): boolean {
+  return Validator.codiceFiscale(fiscalCode).valid;
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
